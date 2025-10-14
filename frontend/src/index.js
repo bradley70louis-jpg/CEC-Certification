e32944f82e81e43b7033b4702a647f0d56463b9c
bradley70louis-jpg <bradley70louis@yahoo.com>
2025-10-14 22:37:48 +0100

    Add base React entry point

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>CEC Certification Platform</h1>
+    <p>Welcome to the frontend.</p>
+  </React.StrictMode>
+);
